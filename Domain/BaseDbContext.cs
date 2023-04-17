e38d1b7d3f1f1143dac4eec5a3024c2af7170aaa
melihsahtiyan <melihsahtiyan@gmail.com>
2023-04-17 21:11:10 +0300

    Add project files.

---
+++ Domain/BaseDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain
+{
+    public class BaseDbContext : DbContext
+    {
+        public BaseDbContext(DbContextOptions<BaseDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseNpgsql("postgresql:MELIH:12345@postgres:54321/VisualProgrammingMidtermProject");
+
+
+
+    }
+}
